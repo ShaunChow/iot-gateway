fe93c6184947e014106760096eb6dba832b9687a
shaunzhou <uranus-neptune@hotmail.com>
2019-12-17 14:17:52 +0800

    [Update] spring-aop will auto EnableAspectJAutoProxy

--- src/main/java/com/shaun/SerialPortClient/service/ChannelCacheService.java
+++ src/main/java/com/shaun/SerialPortClient/service/ChannelCacheService.java
@@ -1,12 +1,10 @@
 package com.shaun.SerialPortClient.service;
 
 import org.springframework.cache.annotation.CachePut;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Service;
 
 import io.netty.channel.Channel;
 
-@EnableAspectJAutoProxy
 @Service
 public class ChannelCacheService {
 
